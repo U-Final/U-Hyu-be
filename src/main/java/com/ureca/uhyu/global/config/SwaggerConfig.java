b5c2f3f0672d8a990c6a3bdf2073efdd2d81f6ad
임동준 <127932430+djlim00@users.noreply.github.com>
2025-07-08 17:38:45 +0900

    UHYU-123 feat: DB 연결 및 Liquibase 설정과 Init changelog 작성

---
+++ src/main/java/com/ureca/uhyu/global/config/SwaggerConfig.java
@@ -0,0 +1,29 @@
+package com.ureca.uhyu.global.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("UHYU API")
+                        .description("요금제 추천 서비스 API 명세서")
+                        .version("v1.0"));
+    }
+
+    @Bean
+    public GroupedOpenApi allApis() {
+        return GroupedOpenApi.builder()
+                .group("All APIs")
+                .pathsToMatch("/**")
+                .build();
+    }
+}
+
