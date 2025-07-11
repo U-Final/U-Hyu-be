340421beec6af7a5066b2fb288e1e7b9715f63f6
SOWON LEE <66356241+Leesowon@users.noreply.github.com>
2025-07-11 09:23:43 +0900

    UHYU-87 chore: 불필요한 주석 제거

--- src/main/java/com/ureca/uhyu/domain/user/controller/UserController.java
+++ src/main/java/com/ureca/uhyu/domain/user/controller/UserController.java
@@ -39,20 +39,19 @@ public class UserController {
      */
     @Operation(summary = "신규 유저 추가 입력 정보 저장", description = "신규 유저 추가 입력 정보 저장, user_role 변경, 토큰 재발급")
     @PostMapping("/extra-info")
-    public CommonResponse<UserOnboardingResponse> onboarding( // 응답 객체에 UserOnboardingResponse dto 객체를 body에 담아 리턴
+    public CommonResponse<UserOnboardingResponse> onboarding(
             @Valid @RequestBody UserOnboardingRequest request, HttpServletResponse response
     ) {
-        // 1. 온보딩 정보 저장 및 유저 권한 변경
+
         Long userId = userService.saveOnboardingInfo(request);
         User user = userService.getUserById(userId);
 
-        // 2. access, refresh 토큰 재발급
         Cookie accessCookie = tokenService.createAccessTokenCookie(String.valueOf(userId), UserRole.USER);
         tokenService.createRefreshToken(user);
 
         response.addCookie(accessCookie);
 
-        return CommonResponse.success(null); // 보내줄 데이터 없음 - 성공했다는 응답만
+        return CommonResponse.success(null);
     }
 
     @Operation(summary = "개인정보 조회", description = "개인정보 조회: 로그인 필요")
