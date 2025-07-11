4484a764e28e6f4519a754c4e9f377a1c3b8f69f
SOWON LEE <66356241+Leesowon@users.noreply.github.com>
2025-07-11 09:24:11 +0900

    UHYU-87 feat: spring security 인증된 사용자 정보 추출 구현

--- src/main/java/com/ureca/uhyu/domain/user/service/UserService.java
+++ src/main/java/com/ureca/uhyu/domain/user/service/UserService.java
@@ -17,6 +17,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
+import org.springframework.security.core.context.SecurityContextHolder;
+import com.ureca.uhyu.domain.auth.dto.CustomOAuth2User;
 
 @Service
 @RequiredArgsConstructor
@@ -28,29 +31,27 @@ public class UserService {
 
     @Transactional
     public Long saveOnboardingInfo(UserOnboardingRequest request) {
-        // 현재 로그인 유저 ID 추출 (예시: SecurityContext 등)
+
         Long currentUserId = getCurrentUserId();
 
-        // 1. 유저 조회
         User user = userRepository.findById(currentUserId)
                 .orElseThrow(() -> new GlobalException(ResultCode.NOT_FOUND_USER));
 
-        // 2. 온보딩 정보 반영
         user.setUserGrade(request.grade());
         user.setUserRole(UserRole.USER); // TMP_USER → USER 변경
 
-//       최근 이용 브랜드 저장
         saveUserBrandData(user, request.recentBrands(), DataType.RECENT);
-
-        // 관심 브랜드 저장
         saveUserBrandData(user, request.interestedBrands(), DataType.INTEREST);
 
         return user.getId();
     }
 
     private Long getCurrentUserId() {
-        // TODO: JWT 또는 SecurityContext에서 userId 추출하는 실제 로직 구현
-        return 1L; // 테스트용 ID (임시)
+        return Optional.ofNullable(SecurityContextHolder.getContext().getAuthentication())
+                .map(auth -> auth.getPrincipal())
+                .filter(principal -> principal instanceof CustomOAuth2User)
+                .map(principal -> ((CustomOAuth2User) principal).getUserId())
+                .orElseThrow(() -> new GlobalException(ResultCode.UNAUTHORIZED));
     }
 
     public GetUserInfoRes findUserInfo(Long userId) {
