6e5cf5672b86d758d273f755254e1547bf88e2a7
etoile0626 <etoile0626@naver.com>
2025-07-17 12:59:24 +0900

    [UHYU-163] refactor: 코드 리뷰에 따른 일부 코드 리팩토링

--- src/main/java/com/ureca/uhyu/domain/auth/repository/TokenRepository.java
+++ src/main/java/com/ureca/uhyu/domain/auth/repository/TokenRepository.java
@@ -5,14 +5,9 @@ import com.ureca.uhyu.domain.user.entity.User;
 import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
-import javax.swing.text.html.Option;
 import java.util.Optional;
 
-@Repository
 public interface TokenRepository extends JpaRepository<Token, Long> {
 
     Optional<Token> findByUser(User user);
--- src/main/java/com/ureca/uhyu/domain/brand/repository/BrandRepository.java
+++ src/main/java/com/ureca/uhyu/domain/brand/repository/BrandRepository.java
@@ -2,11 +2,9 @@ package com.ureca.uhyu.domain.brand.repository;
 
 import com.ureca.uhyu.domain.brand.entity.Brand;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
 public interface BrandRepository extends JpaRepository<Brand, Long>
         , BrandRepositoryCustom{
 
