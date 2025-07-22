1738d41f15374949687cde41aef0cc2ce6b29197
SOWON LEE <66356241+Leesowon@users.noreply.github.com>
2025-07-22 09:41:55 +0900

    [UHYU-78] UHYU-78 fix: 코드 에러 수정

--- src/main/java/com/ureca/uhyu/domain/brand/repository/CategoryRepository.java
+++ src/main/java/com/ureca/uhyu/domain/brand/repository/CategoryRepository.java
@@ -3,7 +3,10 @@ package com.ureca.uhyu.domain.brand.repository;
 import com.ureca.uhyu.domain.brand.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
-    Long findById(long id);
+    Optional<Category> findById(Long id);
+
 }
