13f96bfb8ec0a45ce07327de7f1b88365d3ff4ca
임동준 <127932430+djlim00@users.noreply.github.com>
2025-07-24 16:54:09 +0900

    [UHYU-227] chore: S3 도입 의존성 및 설정 파일 추가

---
+++ src/main/java/com/ureca/uhyu/domain/user/entity/Barcode.java
@@ -0,0 +1,27 @@
+package com.ureca.uhyu.domain.user.entity;
+
+import com.ureca.uhyu.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="barcode")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Builder
+public class Barcode extends BaseEntity {
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", unique = true)
+    private User user;
+
+    private String imageURL;
+
+    public void updateImageUrl(String imageUrl){
+        this.imageURL = imageUrl;
+    }
+}
