7de192a0bb68594ad0a6771a444e1adc1c9b491a
임동준 <127932430+djlim00@users.noreply.github.com>
2025-07-24 16:53:17 +0900

    [UHYU-227] chore:User도메인 디렉토리 분리

--- src/main/java/com/ureca/uhyu/domain/user/repository/actionLogs/ActionLogsRepositoryCustomImpl.java
+++ src/main/java/com/ureca/uhyu/domain/user/repository/actionLogs/ActionLogsRepositoryCustomImpl.java
@@ -1,4 +1,4 @@
-package com.ureca.uhyu.domain.user.repository;
+package com.ureca.uhyu.domain.user.repository.actionLogs;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.ureca.uhyu.domain.brand.entity.Brand;
