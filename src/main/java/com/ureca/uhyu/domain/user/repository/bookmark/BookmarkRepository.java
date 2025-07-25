0c2b0159cee1a62034bcfcecd0d6c6f0b44b2ab8
임동준 <127932430+djlim00@users.noreply.github.com>
2025-07-25 13:19:43 +0900

    [UHYU-227] hotfix: BookmarkRepository 경로관련 에러 해결

--- src/main/java/com/ureca/uhyu/domain/user/repository/bookmark/BookmarkRepository.java
+++ src/main/java/com/ureca/uhyu/domain/user/repository/bookmark/BookmarkRepository.java
@@ -4,13 +4,14 @@ import com.ureca.uhyu.domain.store.entity.Store;
 import com.ureca.uhyu.domain.user.entity.Bookmark;
 import com.ureca.uhyu.domain.user.entity.BookmarkList;
 import com.ureca.uhyu.domain.user.entity.User;
+import com.ureca.uhyu.domain.user.repository.BookmarkRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface BookmarkRepository
-        extends JpaRepository<Bookmark, Long>, BookmarkRepositoryCustom{
+        extends JpaRepository<Bookmark, Long>, BookmarkRepositoryCustom {
     List<Bookmark> findByBookmarkList(BookmarkList bookmarkList);
     Optional<Bookmark> findByBookmarkListAndStore(BookmarkList bookmarkList, Store store);
     boolean existsByBookmarkListUserAndStore(User user, Store store);
