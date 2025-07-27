194f53de0c789607fa4b420c8380ef2320fc11a8
etoile0626 <etoile0626@naver.com>
2025-07-28 08:49:20 +0900

    [UHYU-217] refactor: 코드리뷰에 따른 클래스, 메서드 및 변수명 수정

--- src/main/java/com/ureca/uhyu/domain/admin/dto/response/StatisticsFilterByCategoryRes.java
+++ src/main/java/com/ureca/uhyu/domain/admin/dto/response/StatisticsFilterByCategoryRes.java
@@ -3,13 +3,13 @@ package com.ureca.uhyu.domain.admin.dto.response;
 public record StatisticsFilterByCategoryRes(
         Long categoryId,
         String categoryName,
-        Integer sumCountFilterByCategory
+        Integer sumStatisticsFilterByCategory
 ){
-    public static StatisticsFilterByCategoryRes of(Long categoryId, String categoryName, Integer sumCountFilterByCategory){
+    public static StatisticsFilterByCategoryRes of(Long categoryId, String categoryName, Integer sumStatisticsFilterByCategory){
         return new StatisticsFilterByCategoryRes(
                 categoryId,
                 categoryName,
-                sumCountFilterByCategory
+                sumStatisticsFilterByCategory
         );
     }
 }
