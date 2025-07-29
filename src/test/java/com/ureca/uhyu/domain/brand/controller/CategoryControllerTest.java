76c88ac6f975c51cae1f8310709f7bc265cfa1e5
etoile0626 <etoile0626@naver.com>
2025-07-29 21:43:40 +0900

    [UHYU-295] refactor: 테스트 코드 일부 주석 제거

--- src/test/java/com/ureca/uhyu/domain/brand/controller/CategoryControllerTest.java
+++ src/test/java/com/ureca/uhyu/domain/brand/controller/CategoryControllerTest.java
@@ -1,8 +1,6 @@
 package com.ureca.uhyu.domain.brand.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ureca.uhyu.domain.brand.dto.response.CategoryListRes;
-import com.ureca.uhyu.domain.brand.repository.CategoryRepository;
 import com.ureca.uhyu.domain.brand.service.CategoryService;
 import com.ureca.uhyu.global.config.TestSecurityConfig;
 import org.junit.jupiter.api.BeforeEach;
@@ -10,7 +8,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
